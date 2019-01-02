d0366ddcd188864af3cd8d10f9391b3b781a316d
Kamil Mrzygłód <kamz@demant-technology.com>
2019-01-02 15:04:40 +0100

    Initial `Portal` project

---
+++ Bong.Web/Bong.Portal/Bong.Portal/Controllers/PageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bong.Portal.Controllers
+{
+    public class PageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
