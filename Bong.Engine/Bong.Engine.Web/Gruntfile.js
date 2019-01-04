59a113ec564496a1875725fdfd305f383eb56434
Kamil Mrzygłód <kamz@demant-technology.com>
2019-01-04 15:06:53 +0100

    Added the `build` task for Grunt

--- Bong.Engine/Bong.Engine.Web/Gruntfile.js
+++ Bong.Engine/Bong.Engine.Web/Gruntfile.js
@@ -89,4 +89,5 @@ module.exports = function (grunt) {
 
     grunt.registerTask('default', ['concat', 'sass', 'copy', 'browserify', 'connect', 'watch']);
     grunt.registerTask('reload', ['concat', 'sass', 'copy', 'browserify']);
+    grunt.registerTask('build', ['concat', 'sass', 'copy', 'browserify']);
 };
